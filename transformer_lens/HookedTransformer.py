b49f59f28116c5f2f91bb26398df2559e658b3a8
Ben Millwood <thebenmachine+git@gmail.com>
2024-05-16 16:55:05 +0100

    control with env var for now

--- transformer_lens/HookedTransformer.py
+++ transformer_lens/HookedTransformer.py
@@ -1296,7 +1296,7 @@ class HookedTransformer(HookedRootModule):
         if move_to_device:
             model.move_model_modules_to_device()
 
-        print(f"Loaded pretrained model {model_name} into HookedTransformer")
+        print(f"Loaded pretrained model {official_model_name} into HookedTransformer")
 
         return model
 
--- transformer_lens/fancy_opt_einsum.py
+++ transformer_lens/fancy_opt_einsum.py
@@ -1,5 +1,7 @@
-from fancy_einsum import convert_equation
-from opt_einsum import contract
+import os
+
+import fancy_einsum
+import torch
 
 
 def fancy_opt_einsum(equation: str, *operands):
@@ -13,5 +15,10 @@ def fancy_opt_einsum(equation: str, *operands):
       https://numpy.org/doc/stable/reference/generated/numpy.einsum.html
       https://optimized-einsum.readthedocs.io/en/stable/index.html
     """
-    new_equation = convert_equation(equation)
-    return contract(new_equation, *operands)
+    assert torch.backends.opt_einsum.enabled
+    if "USE_OPT_FOR_FANCY_EINSUM" in os.environ:
+        from opt_einsum import contract
+        new_equation = fancy_einsum.convert_equation(equation)
+        return contract(new_equation, *operands)
+    else:
+        return fancy_einsum.einsum(equation, *operands)
